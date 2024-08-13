20ca128c1ebfa7237f2ac23846a00eaeb37e00e0
Alex K. (pems) <akurzhan@gmail.com>
2024-08-12 18:56:14 -0700

    logfile correction

--- src/run_511org_collect.sh
+++ src/run_511org_collect.sh
@@ -9,5 +9,5 @@ if [ ! -d "$LOGDIR" ]; then
 fi
 
 source "$SRCDIR"/.venv/bin/activate
-python3 "$SRCDIR"/wzd_collect_sf511.py >"$LOGFILE".log 2>"$LOGFILE".err
+python3 "$SRCDIR"/wzd_collect_sf511.py 2>&1 >"$LOGFILE".log
 deactivate
