9019739bcc3fe9f94d5eb391436b5c9b7f30452c
Alex K. (pems) <akurzhan@gmail.com>
2024-08-12 18:41:08 -0700

    bug fixes in run script

--- src/run_511org_collect.sh
+++ src/run_511org_collect.sh
@@ -2,12 +2,12 @@
 
 export SRCDIR="/home/alexk/ucb-wzdx/src"
 export LOGDIR="$SRCDIR"/logs
-export LOGFILE="$LOGDIR"/511org_$(date + '%Y-%m-%d_%H-%M')
+export LOGFILE="$LOGDIR"/511org_$(date +'%Y-%m-%d_%H-%M')
 
 if [ ! -d "$LOGDIR" ]; then
 	mkdir "$LOGDIR"
 fi
 
 source "$SRCDIR"/.venv/bin/activate
-python3 wzd_collect_sf511org.py >"$LOGFILE".log 2>"$LOGFILE".err
+python3 "$SRCDIR"/wzd_collect_sf511.py >"$LOGFILE".log 2>"$LOGFILE".err
 deactivate
