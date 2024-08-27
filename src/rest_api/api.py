97750c6b252e1c8112e29bbc0bf7039dcb9ff797
Ashwin Bardhwaj <ashbardh@gmail.com>
2024-08-27 11:39:06 -0700

    API id record request handler

--- src/rest_api/api.py
+++ src/rest_api/api.py
@@ -3,21 +3,125 @@ RESTful API server for WZ data.
 """
 
 import json
-from flask import Flask, jsonify, request
+import mysql.connector
+from flask import Flask, jsonify, request, g
+from shapely import wkb
+
+# Need to secure this
+SSH_HOST = 'localhost'
+SSH_PORT = 3306
+SSH_USERNAME = 'root'
+SSH_PASSWORD = 'WZDx24'
+SSH_DATABASE = 'wzdb'
 
 
 
 app = Flask(__name__)
 
+def get_db():
+   '''
+   Create singleton instance of the connection object to the database
+   
+   :return: DB connector object
+   '''
+   
+   if 'db' not in g:
+      g.db = mysql.connector.connect(
+         host=SSH_HOST, 
+         port=SSH_PORT, 
+         database=SSH_DATABASE, 
+         user=SSH_USERNAME, 
+         password=SSH_PASSWORD
+      )
+   return g.db
 
+@app.teardown_appcontext
+def close_db(error):
+   '''
+   Closes the connection after every request, even if an error occurs.
+   
+   :param: Error object
+   '''
+   
+   db = g.pop('db', None)
+   if db is not None:
+      db.close()
 
-@app.route('/wzd', methods=['GET'])
+@app.route('/api/wzd', methods=['GET'])
 def get_wzd():
- return jsonify({'data': "data coming soon..."})
+   '''
+   Test Flask route for GET request
+   
+   :return: JSON object 
+   '''
+   
+   return jsonify({'data': "data coming soon..."})
 
+@app.route('/api/wzd/events/id', methods=['GET'])
+def get_wzd_ids():
+   '''
+   GET request for all id values
+   
+   :return: JSON object containing all id Strings
+   '''
+   
+   conn = get_db()
+   mycursor = conn.cursor()
+   mycursor.execute("SELECT id FROM road_event_feature")
+   records = mycursor.fetchall()
+   
+   ids = [r[0] for r in records]
+   
+   return jsonify(ids)
 
-
-
-
+@app.route('/api/wzd/events/<id>', methods=['GET'])
+def get_wzd_record(id):
+   '''
+   Get the coressponding record in GeoJSON from a dataset given an id string.
+   
+   :param: id string
+   
+   :return: formatted JSON record 
+   '''
+   
+   # To-Do: Format data into GeoJSON format and fix event_feature_data conversion
+   
+   conn = get_db()
+   mycursor = conn.cursor()
+   try:
+      mycursor.execute("SELECT * FROM road_event_core_details WHERE road_event_feature_id = %s", (id,))
+      core_details_data = mycursor.fetchone()
+      
+      mycursor.execute("SELECT * FROM road_event_feature WHERE id = %s", (id,))
+      event_feature_data = mycursor.fetchone()
+      
+      mycursor.execute("SELECT * FROM work_zone_event WHERE road_event_feature_id = %s", (id,))
+      work_zone_data = mycursor.fetchone()
+      
+      mycursor.execute("SELECT * FROM worker_presence WHERE road_event_feature_id = %s", (id,))
+      worker_presence_data = mycursor.fetchone()
+      
+      mycursor.execute("SELECT * FROM type_of_work WHERE road_event_feature_id = %s", (id,))
+      work_type_data = mycursor.fetchone()
+      
+      print(len(event_feature_data))
+      converted_event_feature_data = [event_feature_data[0], wkb.loads(bytes(event_feature_data[1])).wkt]
+      
+      
+      # Put this in another method to format into GeoJSON
+      data = {
+         'core_details': core_details_data if core_details_data else None,
+         'event_feature': converted_event_feature_data if event_feature_data else None,
+         'work_zone': work_zone_data if work_zone_data else None,
+         'worker_presence': worker_presence_data if worker_presence_data else None,
+         'type_of_work': work_type_data if work_type_data else None
+      }
+      
+      return jsonify(data)
+   except mysql.connector.Error as err:
+      return jsonify({'error': str(err)}), 500
+   
+   
+   
 if __name__ == '__main__':
    app.run(port=8800)
