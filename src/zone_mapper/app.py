8a889a82e62bb506811cb4271b7c4947c968a46e
Ashwin Bardhwaj <ashbardh@gmail.com>
2024-09-20 12:55:40 -0700

    Able to plot points on map

--- src/zone_mapper/app.py
+++ src/zone_mapper/app.py
@@ -8,7 +8,8 @@ import plotly.io as pio
 pio.renderers.default = "chrome"
 
 #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+#us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+empty_df = pd.DataFrame(columns=['lat', 'lon'])
 
 json_data = requests.get("http://127.0.0.1:8800/api/wzd/events/id").json()
 
@@ -16,28 +17,33 @@ df = pd.DataFrame(json_data)
 
 app = Dash()
 
-fig = px.scatter_map(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig = px.scatter_map(empty_df, lat="lat", lon="lon") 
 fig.update_layout(map_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.update_layout()
+fig.update_layout(map_center={"lat": 37.8715, "lon": -122.2730})
 
 app.layout = [
     html.H1(children='Zone Mapping App', style={'textAlign':'center'}),
     dcc.Dropdown(df.ids.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(figure=fig, style={'width': '1600px', 'height': '800px'})
+    dcc.Graph(id='Work Zone Map', figure=fig, style={'width': '1500px', 'height': '800px'})
 ]
 
 
 
-#@callback(
-#    Output('graph-content', 'figure'),
-#    Input('dropdown-selection', 'value')
-#)
-#def update_graph(value):
-#    geo_json_data = requests.get("http://127.0.0.1:8800/api/wzd/events/id").json()
-#    dff = df[df.country==value]
-#    return px.line(dff, x='year', y='pop')
+@callback(
+    Output('Work Zone Map', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    geo_json_data = requests.get("http://127.0.0.1:8800/api/wzd/events/" + value).json()
+    coords = geo_json_data['geometry']['coordinates']
+    
+    lats = [coord[0] for coord in coords]
+    longs = [coord[1] for coord in coords]
+    
+    fig.update_traces(lat=longs, lon=lats)
+    
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
