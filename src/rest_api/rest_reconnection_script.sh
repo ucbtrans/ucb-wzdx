995ad478a24d60a95527f7585782ff2ded22b454
Alex K. (pems) <akurzhan@gmail.com>
2024-09-22 17:50:46 -0700

    Requirements for REST API

---
+++ src/rest_api/rest_reconnection_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export SRCDIR="/home/alexk/ucb-wzdx/src/rest_api"
+export RESTAPI="$SRCDIR"/api.py
+
+export PORTSTR=`netstat -nlp |grep "128.32.234.154:8800"`
+
+if [ -z "$PORTSTR" ]; then
+	source "$SRCDIR"/.venv/bin/activate
+	echo "Starting WZDx REST API..."
+	nohup python3.8 $RESTAPI &
+	deactivate
+else
+	echo "WZDx REST API is already running..."
+fi
+
