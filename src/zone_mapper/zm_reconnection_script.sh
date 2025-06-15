8e79d72457cb5cc29c90f1947b68900496fa109d
Alex Kurzhanskiy <akurzhan@gmail.com>
2025-06-15 16:16:15 -0700

    venv for zone_mapper

--- src/zone_mapper/zm_reconnection_script.sh
+++ src/zone_mapper/zm_reconnection_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export SRCDIR="/home/alexk/ucb-wzdx/src/zone_mapper"
-export SRCDIR="/home/alex/ucb-wzdx/src/zone_mapper"
+export SRCDIR="/home/alex/ucb-wzdx/src"
 export WZMAPPER="$SRCDIR"/leaflet_app.py
 
 #export PORTSTR=`netstat -nlp |grep "128.32.234.154:8901"`
@@ -10,6 +10,7 @@ export PORTSTR=`netstat -nlp |grep "0.0.0.0:8901`
 if [ -z "$PORTSTR" ]; then
 	cd $SRCDIR
 	source "$SRCDIR"/.venv/bin/activate
+	cd zone_mapper
 	echo "Starting WZ Mapper..."
 	nohup python3 $WZMAPPER >nohup.out &
 	deactivate
