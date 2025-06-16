07142c45bf2415bd697e7059b616c25b5a400525
Alex Kurzhanskiy <akurzhan@gmail.com>
2025-06-15 18:18:28 -0700

    Correction to the source dir

--- src/zone_mapper/zm_reconnection_script.sh
+++ src/zone_mapper/zm_reconnection_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#export SRCDIR="/home/alexk/ucb-wzdx/src/zone_mapper"
-export SRCDIR="/home/alex/ucb-wzdx/src"
+export SRCDIR="/home/alexk/ucb-wzdx/src"
 export WZMAPPER="$SRCDIR"/zone_mapper/leaflet_app.py
 
 #export PORTSTR=`netstat -nlp |grep "128.32.234.154:8901"`
